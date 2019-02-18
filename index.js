217a04fd605a1eb7cdec3a1914dce8fc91e74ea8
Alija Sabic <sabic.alija@gmail.com>
2019-02-18 23:10:48 +0100

    Add debugging statments, cleanup messy parts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const isWin = process.platform === "win32";
-const remote = require("electron").remote;
+const { remote } = require("electron");
 let dirname = __dirname;
 
 if (isWin == true) {
@@ -15,20 +15,42 @@ exports.decorateConfig = config => {
     return config;
   }
 
-  let isLeft = windowControls === "left";
-  let isFlipped = !ifLeft;
+  const titleBarConfig = {
+    iconSize: "16px",
+    height: "26px",
+    headerBg: "#2f343f",
+    closeBg: "#f25056",
+    minimizeBg: "#fac536",
+    maximizeBg: "#39ea49",
+    borderColor: "#000"
+  };
 
-  const conf = Object.assign({}, config, {
+  let isLeft = windowControls === "left";
+  let isFlipped = isLeft;
+
+  const radius = 20;
+  const padding = 5;
+
+  const controlGroupWidth = 4 * padding + 3 * radius;
+  const controlGroupHeight = 2 * padding + radius;
+
+  const closeColor = "#f25056";
+  const minimizeColor = "#fac536";
+  const maximizeColor = "#39ea49";
+
+  return Object.assign({}, config, {
     css: `
       ${config.css || ""}
       .header_windowHeader {
-        height: 22px;
-        left: ${isLeft ? "57px" : "0"};
-        width: calc(100% - 56px);
+        height: ${controlGroupHeight}px;
+        left: ${isLeft ? `${controlGroupWidth}px` : "0"};
+        // width: calc(100% - ${controlGroupWidth}px);
+        width: 100%;
       }
+      // .header_shape {
       // .header_windowControls {
-        .header_shape {
-        display: none;
+      .header_windowHeader {
+        visibility: hidden;
       }
       .header_appTitle {
         margin-left: -56px;
@@ -37,8 +59,8 @@ exports.decorateConfig = config => {
         position: fixed;
         top: 0;
         ${isLeft ? "left: 0;" : "right: 0;"}
-        height: 22px;
-        width: 56px;
+        height: ${controlGroupHeight}px;
+        width: ${controlGroupWidth}px;
       }
       .mac_actions {
         position: absolute;
@@ -50,41 +72,50 @@ exports.decorateConfig = config => {
       .mac_header .mac_close,
       .mac_header .mac_minimize,
       .mac_header .mac_maximize {
-        width: 12px;
-        height: 12px;
+        z-index: 1000;
+        width:  ${radius}px;
+        height: ${radius}px;
         border-radius: 50%;
         position: absolute;
         top: 5px;
         background-posiition: -6px;
       }
       .mac_header .mac_close {
-        background-color: #f25056;
-        left: ${isFlipped ? "5px" : "40px"};
+        background-color: ${closeColor};
+        ${isFlipped ? "left" : "right"}: ${padding}px;
       }
       .mac_header .mac_close:hover {
 
       }
       .mac_header .mac_minimize {
-        background-color: #fac536;
-        left: 23px;
+        background-color: ${minimizeColor};
+        left: ${2 * padding + radius}px;
       }
       .mac_header .mac_minimize:hover {
 
       }
       .mac_header .mac_maximize {
-        background-color: #39ea49;
-        left: ${isFlipped ? "40px" : "5px"};
+        background-color: ${maximizeColor};
+        ${isFlipped ? "right" : "left"}: ${padding}px;
       }
       .mac_header .mac_maximize:hover {
         
       }
     `
   });
-
-  return conf;
 };
 
 exports.decorateHeader = (Hyper, { React }) => {
+  // React.Children.map(Hyper, x => {
+  //   console.log(x);
+  // });
+
+  let a = React.Children.toArray(Hyper);
+  console.log(a);
+
+  let w = remote.getCurrentWindow();
+  console.log(w);
+
   return class extends React.Component {
     constructor(props) {
       super(props);
@@ -127,10 +158,25 @@ exports.decorateHeader = (Hyper, { React }) => {
             { className: "mac_header" },
             React.createElement(
               "div",
-              { className: "mac_header" },
-              React.createElement("span", { className: "mac_close", onClick: this.closeApp }),
-              React.createElement("span", { className: "mac_minimize", onClick: this.minimizeApp }),
-              React.createElement("span", { className: "mac_maximize", onClick: this.maximizeApp })
+              { className: "mac_actions" },
+              React.createElement("span", {
+                className: "mac_close",
+                onClick: () => {
+                  this.closeApp;
+                }
+              }),
+              React.createElement("span", {
+                className: "mac_minimize",
+                onClick: () => {
+                  this.minimizeApp;
+                }
+              }),
+              React.createElement("span", {
+                className: "mac_maximize",
+                onClick: () => {
+                  this.maximizeApp;
+                }
+              })
             )
           )
         })
